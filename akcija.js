c8301ce356c9a3ba4479f01bb43db247950d9d95
basicallyela <114830703+basicallyela@users.noreply.github.com>
2022-11-05 21:48:23 +0100

    Update akcija.js

--- akcija.js
+++ akcija.js
@@ -1,4 +1,1 @@
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
+
